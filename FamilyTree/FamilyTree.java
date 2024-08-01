90332fff68890fa05026fee23e67f5f4648156fd
Дмитрий Левыкин <dmlegasy@dmail.com>
2024-08-01 13:20:30 +0300

    Was added packeges

---
+++ FamilyTree/FamilyTree.java
@@ -0,0 +1,45 @@
+package FamilyTree;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import Human.Person;
+
+public class FamilyTree {
+    List<Person> family;
+
+    public FamilyTree() {
+        this(new LinkedList<>());
+    }
+
+    public FamilyTree(List<Person> family) {
+        this.family = family;
+    }
+
+    public boolean add(Person person) {
+        if (person == null) {
+            return false;
+        }
+        if (!family.contains(person)) {
+            family.add(person);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        try {
+            System.out.println("Количество людей в семейном древе: " + family.size());
+            System.out.println(" ");
+            for (int i = 0; i < family.size(); ++i) {
+                System.out.println("Родитель " + family.get(i));
+                System.out.println(family.get(i).setChild(family.get(i + 1)));
+                System.out.println(' ');
+            }
+        } catch (IndexOutOfBoundsException e) {
+            return " ";
+        }
+        return " ";
+    }
+}
---
+++ Human/Person.java
@@ -0,0 +1,58 @@
+package Human;
+
+import java.time.LocalDate;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private Gender gender;
+    private Person mother;
+    private Person father;
+    private Person child;
+    private LocalDate dateBirth;
+
+    public Person(String firstName, String lastName, Gender gender, 
+     LocalDate dateBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.dateBirth = dateBirth;
+    }
+
+    public String setMother(Person mother) {
+        this.mother = mother;
+        return " Мать [ " + mother + " ]";
+    }
+
+    public String setFather(Person father) {
+        this.father = father;
+        return " Отец [ " + father + " ]";
+    }
+
+    public String setChild(Person child) {
+        this.child = child;
+        return " Ребенок [ " + child + " ]";
+    }
+
+    public String getFirstName() {
+        return "Имя: " + firstName;
+    }
+
+    public String getLastName() {
+        return "Фамилия: " + lastName;
+    }
+
+    public String getGender() {
+        return "Пол: " + gender;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Имя: " + firstName + ", "
+                + "Фамилия: " + lastName + ", "
+                + "Пол: " + gender + ','
+                + " Дата рожденния: " + dateBirth;
+    } 
+
+}
